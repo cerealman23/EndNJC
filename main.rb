8351579604048ac7a294463b17cff64d4ba0beea
Malcolm Kahora <mango22mjk@gmail.com>
2022-10-21 12:53:43 -0400

    Proper scale

--- main.rb
+++ main.rb
@@ -8,7 +8,6 @@ http = Net::HTTP.new(url.host, url.port)
 http.use_ssl = true
 http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-
 request = Net::HTTP::Get.new(url)
 request["x-rapidapi-host"] = 'random-facts2.p.rapidapi.com'
 request["x-rapidapi-key"] = 'your-rapidapi-key'
