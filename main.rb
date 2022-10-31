d978b87fb3a6e0896c3d751565dee060483989d4
Malcolm Kahora <mango22mjk@gmail.com>
2022-10-30 23:48:35 -0400

    working cholopath map

--- main.rb
+++ main.rb
@@ -14,7 +14,7 @@ class FormData
 
 
   def data_based_on_year year
-    final_struct = {} 
+    final_struct = [] 
     csv = CSV.read("incarceration_trends.csv")
     total = 0
 
@@ -23,16 +23,29 @@ class FormData
     csv.each do | row |
 
       if row[1] == "2012"
-        final_struct[extract_county(row[4]).to_sym] = row[5]
+        final_struct << [extract_county(row[4]), row[5]]
       end
     end
 
     # end
 
-      final_struct.length
+      final_struct
 
+  end
+  def write_to_csv data
+    CSV.open("./county-prison.csv", "wb") do | csv |
+
+
+      csv << ["county", "jail"] 
+      data.each do | row |
+
+        csv << [row[0], row[1]]
+
+      end
+
+    end
   end
 end
 
 form = FormData.new
-pp form.data_based_on_year "2012"
+form.write_to_csv(form.data_based_on_year "2012")
